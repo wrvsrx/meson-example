91624f953f70f6562a36c40267fb33982c63894f
wrvsrx <wrvsrx@outlook.com>
2024-05-30 16:20:42 +0800

    feat: add code using cpp 20 feature

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <Eigen/Eigen>
 #include <iostream>
+// some cpp 20 stuff for standard test
+#include <ranges>
 
 auto main() -> int {
   std::cout << "Hello world!" << std::endl;
 
   auto const a = Eigen::Vector<double, 3>{0.0, 1.0, 2.0};
   std::cout << a << std::endl;
+
+  for (auto const i : std::views::iota(0u, 10u)) {
+    std::cout << i << ' ';
+  }
+  std::cout << std::endl;
+  return 0;
 }
